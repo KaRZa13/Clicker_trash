ddb021d6bee11225ae1897cef6093b6dd24dcbfb
Rafael MURO <rafael.francisco.muro@gmail.com>
2024-11-15 21:31:29 +0100

    Nouvelle structure

--- js/script.js
+++ js/script.js
@@ -1,105 +1,74 @@
+const gameData = {
+    speed: {
+        count: 0,
+        rate: 1,
+    },
+    clinic:{
+        count:0,
+        rate:0.001,
+        cost: 15,
+    }
+}
+
 // Cliniques
 const buyClinic = document.querySelector("#buyClinic")
 const clinicPrice = document.querySelector("#clinicPrice")
 const labelClinic = document.querySelector("#labelClinic")
-let clinicCost = 15
-let clinic = 0
-
-// Gravedigger
-const buyGravedigger = document.querySelector("#buyGrave")
-const gravediggerPrice = document.querySelector("#gravePrice")
-const gravediggerLabel = document.querySelector("#labelGrave")
-let gravediggerCost = 100
-let gravedigger = 0
-
-// Necromencien
-const buyNecro = document.querySelector("#buyNecro")
-const necroPrice = document.querySelector("#necroPrice")
-const necroLabel = document.querySelector("#labelNecro")
-let necroCost = 1100
-let necro = 0
+const upClinic = document.querySelector("#upClinic")
 
 // Upgrades
-
 const upgrade = document.querySelector('#upClick')
-
 
 
 // Incrémentation
 const incrementButton = document.querySelector("#heart")
 const reset = document.querySelector("#resetScore") // dev only
 let scoreLabel = document.querySelector("#scoreLabel")
-const heartsPerSecLabel = document.querySelector("#heartsPerSecCounter")
-let increment = 1
-let score = 100000
-let heartsPerSecond = 0
+const kilometerPerSecLabel = document.querySelector("#heartsPerSecCounter")
+let kilometerPerSecond = 0
 
 const updateHeartsPerSecDisplay = () => {
-    heartsPerSecLabel.textContent = (heartsPerSecond*100).toFixed(1)
+    kilometerPerSecLabel.textContent = (kilometerPerSecond*100).toFixed(1)
 }
 
 // Fonction pour incrémenter automatiquement
-const passiveIncrement = () => {
-    score += heartsPerSecond
-    scoreLabel.textContent = Math.round(score)
-}
-
-setInterval(passiveIncrement, 10)
+setInterval(passiveIncrement = () => {
+    gameData.speed.count += kilometerPerSecond
+    scoreLabel.textContent = Math.round(gameData.speed.count)
+}, 10)
 
 // Incrémentation manuelle
 incrementButton.addEventListener("click", () => {
-    score += increment
-    scoreLabel.textContent = Math.round(score)
+    gameData.speed.count += gameData.speed.rate
+    scoreLabel.textContent = Math.round(gameData.speed.count)
 });
 
 // Upgrade incrémentation
 upgrade.addEventListener("click",() => {
-    increment *= 2
+    gameData.speed.rate *= 2
     upgrade.disabled = true
 })
 
 // Reset le score (dev only)
 reset.addEventListener("click", () => {
-    score = 0
-    scoreLabel.textContent = Math.round(score)
+    gameData.speed.count = 0
+    scoreLabel.textContent = Math.round(gameData.speed.count)
 });
 
 // Acheter une clinique 
 buyClinic.addEventListener("click", () => {
-
-    if (score >= clinicCost) {
-        score -= clinicCost
-        clinic++
-        labelClinic.textContent = clinic
-        heartsPerSecond += 0.001
-        clinicCost = Math.round(clinicCost *= 1.15)
-        clinicPrice.textContent = clinicCost
+    if (gameData.speed.count >= gameData.clinic.cost) {
+        gameData.speed.count -= gameData.clinic.cost
+        gameData.clinic.count++
+        labelClinic.textContent = gameData.clinic.count
+        kilometerPerSecond += gameData.clinic.rate
+        gameData.clinic.cost = Math.round(gameData.clinic.cost *= 1.15)
+        clinicPrice.textContent = gameData.clinic.cost
         updateHeartsPerSecDisplay()
     }
 })
 
-// Engager un fossoyeur 
-buyGravedigger.addEventListener("click", () => {
-    if (score >= gravediggerCost) {
-        score -= gravediggerCost
-        gravedigger++
-        gravediggerLabel.textContent = gravedigger
-        heartsPerSecond += 0.01
-        gravediggerCost = Math.round(gravediggerCost *= 1.15)
-        gravediggerPrice.textContent = gravediggerCost
-        updateHeartsPerSecDisplay()
-    }
-})
+upClinic.addEventListener("click", () => {
+    gameData.clinic.rate *= 2
 
-// Engager un nécromencien
-buyNecro.addEventListener("click", () => {
-    if (score >= necroCost) {
-        score -= necroCost
-        necro++
-        necroLabel.textContent = necro
-        heartsPerSecond += 0.08
-        necroCost = Math.round(necroCost *= 1.15)
-        necroPrice.textContent = necroCost
-        updateHeartsPerSecDisplay()
-    }
 })
